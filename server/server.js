bce410a8be09aee5f68fed63557aa3aa858d3891
vartanyanE <mike.vartanyan@yahoo.com>
2021-02-28 08:24:32 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 //specify at what endpoint do we want to hit our routes
 app.use("/api/data", allThingsRoutes);
